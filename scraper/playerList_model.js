943000e01062dfb97903951e163da9cc0bc9329a
Shray bansal <bansalshray@gmail.com>
2016-09-24 19:20:43 -0700

    finished playerData schema

--- scraper/playerList_model.js
+++ scraper/playerList_model.js
@@ -5,9 +5,16 @@ var connector = mongoose.connect('mongodb://localhost/ballr');
 //Schema for all data of each player
 
 const playerSchema = new Schema({
-  firstname: String,
-  lastName: String,
-
+  first_name: String,
+  last_name: String,
+  fg_percent: Number,
+  ft_percent: Number,
+  three_percent: Number,
+  assists: Number,
+  steals: Number,
+  blocks: Number,
+  turnovers: Number,
+  points: Number
 });
 
 const playerData = mongoose.model('playerData', playerSchema);
