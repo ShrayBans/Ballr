69ea75d8fe85bbd364a3b287c2d7d8856bb90997
Shray bansal <bansalshray@gmail.com>
2016-09-26 00:15:46 -0700

    added search bar - doesn't fully work

--- app/store.js
+++ app/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import promise from 'redux-promise-middleware';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-import reducers from './reducers';
+import reducers from './reducers/index.js';
 
 const middleware = applyMiddleware(promise(), thunk, logger());
 export default createStore(reducers, middleware);
