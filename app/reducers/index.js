9f3629c8ad7424535e2b4dcfb237c293db8dc5b7
Shray bansal <bansalshray@gmail.com>
2016-09-26 17:08:57 -0700

    added winner message

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,9 +1,11 @@
 import {combineReducers} from 'redux';
 
-import players from './playerReducers';
+import players from './winnerReducer';
+import winner from './playerReducers';
 import button from './buttonReducer';
 
 export default combineReducers({
 	players,
-	button
+	button,
+	winner
 });
